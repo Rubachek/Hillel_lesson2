07d4d9bfd8b7c79f7f7c3083eac534a66c5e7f0f
дмитрий <rubachek1990@gmail.com>
2023-11-12 12:58:18 +0200

    homrwork

--- pages/registration_facade.py
+++ pages/registration_facade.py
@@ -14,15 +14,18 @@ class RegistrationFacade(BaseFacade):
         self.driver.get("https://guest:welcome2qauto@qauto2.forstudy.space/")
 
     @allure.step("Register user")
-    def registration_user(self, name, last_name, email, password, repeat_password):
+    # заменить на обьект
+    def registration_user(self, user):
         self._main_page.sing_up_button().click()
-        self._registration_form_page.name_field.send_keys(name)
-        self._registration_form_page.last_name_field.send_keys(last_name)
-        self._registration_form_page.email_field.send_keys(email)
-        self._registration_form_page.password_field().send_keys(password)
-        self._registration_form_page.reenter_password_field().send_keys(repeat_password)
+        self._registration_form_page.name_field.send_keys(user.name)
+        self._registration_form_page.last_name_field.send_keys(user.last_name)
+        self._registration_form_page.email_field.send_keys(user.email)
+        self._registration_form_page.password_field().send_keys(user.password)
+        self._registration_form_page.reenter_password_field().send_keys(user.password)
         allure.attach(self.driver.get_screenshot_as_png(), name="screen", attachment_type=AttachmentType.PNG)
         self._registration_form_page.register_button().click()
+        # self.uaser
+
 
     def check_is_user_logged_in(self):
         return len(self._garage_page.empty_garage()) > 0
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,23 +2,34 @@ import logging
 
 import pytest
 import requests
+from data import users
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from pages.registration_facade import RegistrationFacade
 
+# from hillel_auto_2023.data.users import UserCreator, RegistrationTestsDataPath
+
 
 def user_data():
     """ This function should be replaced with a new fixture registration_user
     fixture should return only one correct user!!!"""
-    user_email = "sytischenko@gmail.com"
-    user_password = "N9Xb46SCbgd8wy!"
+    user_email = "sytischenk1o@gmail.com"
+    user_password = "N9Xb46SC2bgd8wy!"
     user_to_login = {
         "email": user_email,
         "password": user_password,
         "remember": False
     }
     return user_to_login
+    # # user_password = "N9Xb46SCbgd8wy!"
+    # user.password
+    # user.first_name
+    # user.last_name
+    # user.
+@pytest.fixture()
+def user():
+    yield users.UserCreator.registration_users(users.RegistrationTestsDataPath )
 
 
 @pytest.fixture
